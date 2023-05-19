d030fc62de7d307ca830021d4d1a702d03441923
kik4444 <7779637+kik4444@users.noreply.github.com>
2023-05-19 15:07:33 +0300

    Add get_socket_name

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 
+use interprocess::local_socket::NameTypeSupport;
+
 pub mod commands;
 pub mod errors;
 pub mod process_scanner;
@@ -8,4 +10,13 @@ pub mod structures;
 
 pub fn get_config_dir() -> Option<PathBuf> {
     dirs::config_dir().map(|dir| dir.join("simple-process-tracker"))
+}
+
+pub fn get_socket_name() -> &'static str {
+    use NameTypeSupport::*;
+
+    match NameTypeSupport::query() {
+        OnlyPaths => "/tmp/simple-process-tracker.sock",
+        OnlyNamespaced | Both => "@simple-process-tracker.sock",
+    }
 }
